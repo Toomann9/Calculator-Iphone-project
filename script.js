c8d01b2b0a9853240b7622cd26391cd47e6b261a
Toomann9 <ntoomann9@gmail.com>
2023-03-20 10:00:14 +0000

    JavaScript in progress

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ for(let i = 0; i < buttons.length; i++){
     let theClikedItem = buttons[i].addEventListener('click', displayFn)
 }
 
+function multiply(n1, n2){
+    product = (n1 * n2);
+    return product;
+}
+
 function displayFn(e){
     if (e.target.innerText == "AC"){
         display.innerText = "";
@@ -15,15 +20,26 @@ function displayFn(e){
         display.innerText = display.innerText.slice(0, -1);
     }
     
-    else if (e.target.innerText == "*"){
-        display.innerText = display.innerText
-    }
+    // else if (e.target.innerText == "*"){
+    //     display.innerText = eval(display.innerText * display.innerText);
+    // }
     
     else if (e.target.innerText == "="){
-        display.innerText = eval(display.innerText);
+        try {
+            if(display.innerText.indexOf("%")!= -1){
+                display.innerText = display.innerText.slice(0, -1) / 100;
+            } else {
+                display.innerText = eval(display.innerText);
+            }
+
+        } catch {
+            display.innerText = "Error!"
+        }
+
     }
     
     else {
         display.innerText += e.target.innerText;
     }
 }
+
